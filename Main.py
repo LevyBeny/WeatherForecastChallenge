010f924ea548dce14695010fb22b141221d95b9a
levybeny <ySsdsp9j>
2018-05-03 16:55:13 +0300

    starting Ben

--- Main.py
+++ Main.py
@@ -1,8 +1,25 @@
 import PreProcess as preproc
-
+import os
 train_dir = "./data/train"
 test_dir = "./data/test"
 
-file_name = "train_Humidity.csv"
+data_names = ["humidity", "maxTemp","minTemp","wind"]
 
-preproc.read_data(train_dir,file_name)
+all_sets=[]
+
+data = preproc.read_data(train_dir, "train_Humidity.csv")
+all_sets.append(preproc.merge_time1_time2(data,"humidity"))
+
+data = preproc.read_data(train_dir, "train_minTemp.csv")
+all_sets.append(preproc.merge_time1_time2(data,"minTemp"))
+
+data = preproc.read_data(train_dir, "train_maxTemp.csv")
+all_sets.append(preproc.merge_time1_time2(data,"maxTemp"))
+
+data = preproc.read_data(train_dir, "train_Wind.csv")
+all_sets.append(preproc.merge_time1_time2(data,"Wind"))
+
+merged = preproc.merge_data(all_sets[0], all_sets[1])
+for i in range(2,len(all_sets)):
+    merged = preproc.merge_data(merged,all_sets[i])
+
--- PreProcess.py
+++ PreProcess.py
@@ -13,10 +13,48 @@ def read_data(dir_path, file_name):
 
     data = pd.read_csv(dir_path + "/" + file_name, parse_dates=parse_dates,
                        date_parser=_date_parser)
-    data.set_index()
+    del data['Basis_date']
+    return data
 
-def merge_time1_time2(data):
-    columns_names = list(data)
-    columns_to_remove=['Time','Basis_date','Validity_date','City']
 
-    new_data = pd.DataFrame()
+def merge_time1_time2(data, data_name):
+    columns_to_duplicate = list(data)
+    columns_to_remove = ['Time', 'Validity_date', 'City', 'Persist_value_' + data_name]
+    columns_common = ['Validity_date', 'City', 'Persist_value_' + data_name]
+    for column in columns_to_remove:
+        columns_to_duplicate.remove(column)
+
+    duplicated = []
+    for column in columns_to_duplicate:
+        duplicated.append(column + "_1")
+        duplicated.append(column + "_2")
+
+    new_columns = ['Validity_date', 'City', 'Persist_value_' + data_name] + duplicated
+    new_data = pd.DataFrame(columns=new_columns)
+
+    for index, row in data.iterrows():
+        new_row = {}
+        instance_index = new_data.index[(new_data['City'] == row['City']) & (new_data['Validity_date'] == row['Validity_date'])]
+
+
+        if len(instance_index) == 0:
+            for column in columns_common:
+                new_row[column] = row[column]
+
+            for column in columns_to_duplicate:
+                new_row[column + "_" + str(row["Time"])] = row[column]
+            new_data = new_data.append(new_row, ignore_index=True)
+
+        else:
+            for column in columns_to_duplicate:
+                new_data.at[instance_index,column + "_" + str(row["Time"])] = row[column]
+    return new_data
+
+def merge_data(data, to_add):
+    common_columns = ['Validity_date', 'City']
+
+    columns_to_append = list(to_add).remove(common_columns)
+    data = data.sort(columns=common_columns, axis=0)
+    to_add = to_add.sort(columns=common_columns, axis=0)
+    to_add = pd.DataFrame(to_add[columns_to_append])
+    return pd.concat([data, to_add], axis=1)
